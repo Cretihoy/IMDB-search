d8438aca2b6e5eaa645c361a27267cb4a0209d15
cretihoy <sergzhio238@gmail.com>
2023-04-21 20:24:51 +0300

    fix logic

--- app/src/main/java/com/example/imdbsearch/mvp/MovieListActivity.kt
+++ app/src/main/java/com/example/imdbsearch/mvp/MovieListActivity.kt
@@ -5,13 +5,9 @@ import android.widget.Button
 import android.widget.EditText
 import androidx.recyclerview.widget.RecyclerView
 import com.example.data.model.MovieModel
-import com.example.data.service.MovieServiceBuilder
 import com.example.imdbsearch.R
 import com.example.imdbsearch.movieListRecycler.MovieListAdapter
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 import moxy.MvpAppCompatActivity
 import moxy.ktx.moxyPresenter
 import javax.inject.Inject
@@ -20,11 +16,10 @@ import javax.inject.Provider
 @AndroidEntryPoint
 class MovieListActivity : MvpAppCompatActivity(), MovieListView {
 
-    var result: Any? = null
-    val recycler: RecyclerView by lazy { findViewById(R.id.movie_list_recycler) }
-    val buttonSearch: Button by lazy { findViewById(R.id.movie_list_button) }
-    val editText: EditText by lazy { findViewById(R.id.movie_list_edit_text) }
-    val adapter = MovieListAdapter()
+    private val recycler: RecyclerView by lazy { findViewById(R.id.movie_list_recycler) }
+    private val buttonSearch: Button by lazy { findViewById(R.id.movie_list_button) }
+    private val editText: EditText by lazy { findViewById(R.id.movie_list_edit_text) }
+    private val adapter = MovieListAdapter()
 
     @Inject
     lateinit var presenterProvider: Provider<MovieListPresenter>
@@ -35,12 +30,10 @@ class MovieListActivity : MvpAppCompatActivity(), MovieListView {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie_list)
 
-        loadMovies()
-    }
+        recycler.adapter = adapter
 
-    private fun loadMovies() {
-        CoroutineScope(Dispatchers.Main).launch {
-            result = MovieServiceBuilder.getClient().getMovieByName("Inception")
+        buttonSearch.setOnClickListener {
+            presenter.onSearchButtonClicked(editText.text.toString())
         }
     }
 
