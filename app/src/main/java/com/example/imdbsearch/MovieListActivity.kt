6ca938f43781a51e5803dd88b85b443d36daae32
cretihoy <sergzhio238@gmail.com>
2023-04-21 20:04:47 +0300

    rename activity

--- app/src/main/java/com/example/imdbsearch/MovieListActivity.kt
+++ app/src/main/java/com/example/imdbsearch/MovieListActivity.kt
@@ -7,7 +7,7 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
-class MainActivity : AppCompatActivity() {
+class MovieListActivity : AppCompatActivity() {
 
     var result: Any? = null
 
