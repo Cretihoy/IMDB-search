5c5e32220666e3fd5de3770fd26b6222ffcd13f7
cretihoy <sergzhio238@gmail.com>
2023-04-21 18:02:58 +0300

    add fun for get json

--- app/src/main/java/com/example/imdbsearch/MainActivity.kt
+++ app/src/main/java/com/example/imdbsearch/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.imdbsearch
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.data.service.MovieServiceBuilder
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    var result: Any? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        loadMovies()
+    }
+
+    private fun loadMovies() {
+        CoroutineScope(Dispatchers.Main).launch {
+            result = MovieServiceBuilder.getClient().getMovieByName("Inception")
+        }
     }
 }
