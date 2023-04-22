a9dadad1a61838e0596a5dde582576e4e32c884f
cretihoy <sergzhio238@gmail.com>
2023-04-22 18:53:31 +0300

    add mapper in movie repository impl

--- data/src/main/java/com/example/data/mapper/MovieMapper.kt
+++ data/src/main/java/com/example/data/mapper/MovieMapper.kt
@@ -1,13 +1,18 @@
 package com.example.data.mapper
 
 import com.example.data.model.MovieDto
+import com.example.data.model.MovieResponse
 import com.example.domain.model.MovieModel
 import javax.inject.Inject
 
 class MovieMapper
 @Inject constructor() {
 
-    fun map(dto: MovieDto): MovieModel {
+    fun map(response: MovieResponse): List<MovieModel> {
+        return response.results.map { map(it) }
+    }
+
+    private fun map(dto: MovieDto): MovieModel {
         return MovieModel(
             id = dto.id,
             posterUrl = dto.image,
--- data/src/main/java/com/example/data/repository/MovieRepositoryImpl.kt
+++ data/src/main/java/com/example/data/repository/MovieRepositoryImpl.kt
@@ -1,15 +1,18 @@
 package com.example.data.repository
 
-import com.example.data.model.MovieDto
+import com.example.data.mapper.MovieMapper
 import com.example.data.service.MovieService
+import com.example.domain.model.MovieModel
+import com.example.domain.repository.MovieRepository
 import javax.inject.Inject
 
 class MovieRepositoryImpl
 @Inject constructor(
-    private val service: MovieService
-) {
+    private val service: MovieService,
+    private val mapper: MovieMapper
+) : MovieRepository {
 
-    suspend fun getMoviesByName(name: String): List<MovieDto> {
-        return service.getMovieByName(name).results
+    override suspend fun getMovieByName(name: String): List<MovieModel> {
+        return mapper.map(service.getMovieByName(name))
     }
 }
