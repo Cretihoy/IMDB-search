fe1dd1a5a531fcd848d3ec20c681f30d86aa0c1a
cretihoy <sergzhio238@gmail.com>
2023-04-30 19:18:04 +0300

    add new mapper

--- data/src/main/java/com/example/data/repository/MovieRepositoryImpl.kt
+++ data/src/main/java/com/example/data/repository/MovieRepositoryImpl.kt
@@ -1,7 +1,9 @@
 package com.example.data.repository
 
+import com.example.data.mapper.CurrentMovieMapper
 import com.example.data.mapper.MovieMapper
 import com.example.data.service.MovieService
+import com.example.domain.model.CurrentMovieModel
 import com.example.domain.model.MovieModel
 import com.example.domain.repository.MovieRepository
 import javax.inject.Inject
@@ -9,10 +11,15 @@ import javax.inject.Inject
 class MovieRepositoryImpl
 @Inject constructor(
     private val service: MovieService,
-    private val mapper: MovieMapper
+    private val movieMapper: MovieMapper,
+    private val currentMovieMapper: CurrentMovieMapper
 ) : MovieRepository {
 
-    override suspend fun getMovieByName(name: String): List<MovieModel> {
-        return mapper.map(service.getMovieByName(name))
+    override suspend fun getMoviesByName(name: String): List<MovieModel> {
+        return movieMapper.map(service.getMoviesByName(name))
+    }
+
+    override suspend fun getMovieById(id: String): CurrentMovieModel {
+        return currentMovieMapper.map(service.getMovieById(id))
     }
 }
--- domain/src/main/java/com/example/domain/repository/MovieRepository.kt
+++ domain/src/main/java/com/example/domain/repository/MovieRepository.kt
@@ -1,8 +1,11 @@
 package com.example.domain.repository
 
+import com.example.domain.model.CurrentMovieModel
 import com.example.domain.model.MovieModel
 
 interface MovieRepository {
 
-    suspend fun getMovieByName(name: String): List<MovieModel>
+    suspend fun getMoviesByName(name: String): List<MovieModel>
+
+    suspend fun getMovieById(id: String): CurrentMovieModel
 }
